d74c445a7dde527e34976dc54cb7d29577d2a474
Anoobislazy <shin56858705@gmail.com>
2024-02-16 12:05:39 +0900

    Effect UPG

--- js/mod.js
+++ js/mod.js
@@ -38,7 +38,7 @@ function canGenPoints(){
 }
 
 // Calculate points/sec!
-function getPointGen() {
+function getPointGen() {if (hasUpgrade('p', 11)) gain = gain.times(2)
 	if(!canGenPoints())
 		return new Decimal(0)
 
